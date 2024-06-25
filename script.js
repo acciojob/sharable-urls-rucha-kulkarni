7c6001898c8b85e790537a821cc1d1d7a22c65c5
rucha-kulkarni <118054484+rucha-kulkarni@users.noreply.github.com>
2024-06-25 20:27:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// your code here
+document.getElementById('button')
+		 .addEventListener('click', function(){
+let name = document.getElementById('name').value;
+let year = document.getElementById('year').value;
+const queryString = `https://localhost:8080/?name=${encodeURIComponent(name)}&year=${year}`;
+});
+document.getElementById('url').textContent = queryString;
